92a72633880b52fae889f5681e3ac19b2678262a
issa ouedraogo <i.ouedraogo49@yahoo.fr>
2019-02-10 20:44:12 -0800

    Importing Project

---
+++ LauchPad/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace LauchPad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstFolders = new System.Windows.Forms.ListBox();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(693, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lauch Pad for Folder and Files";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(278, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Recently Accessed Folders";
+            // 
+            // lstFolders
+            // 
+            this.lstFolders.FormattingEnabled = true;
+            this.lstFolders.Location = new System.Drawing.Point(7, 44);
+            this.lstFolders.Name = "lstFolders";
+            this.lstFolders.Size = new System.Drawing.Size(508, 108);
+            this.lstFolders.TabIndex = 2;
+            this.lstFolders.SelectedIndexChanged += new System.EventHandler(this.lstFolders_SelectedIndexChanged);
+            this.lstFolders.DoubleClick += new System.EventHandler(this.lstFolders_SelectedIndexChanged);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNew.Location = new System.Drawing.Point(560, 6);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(125, 48);
+            this.btnNew.TabIndex = 3;
+            this.btnNew.Text = "New ";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(328, 31);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Recently Accessed Files";
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.Location = new System.Drawing.Point(0, 242);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(693, 303);
+            this.lstFiles.TabIndex = 5;
+            this.lstFiles.SelectedIndexChanged += new System.EventHandler(this.lstFiles_SelectedIndexChanged);
+            this.lstFiles.DoubleClick += new System.EventHandler(this.lstFiles_SelectedIndexChanged);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(612, 148);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(73, 32);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Controls.Add(this.lstFolders);
+            this.panel1.Controls.Add(this.btnNew);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(693, 193);
+            this.panel1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 545);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstFolders;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ LauchPad/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace LauchPad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> folderList = new List<string>();
+        List<string> fileList = new List<string>();
+
+        public void RessourceToListAndListBox(StreamReader sr, ListBox lb, List<string> ls)
+        {
+            lb.Items.Clear();
+            ls.Clear();
+            while(sr.Peek() > -1)
+            {
+                ls.Add(sr.ReadLine());
+            }
+            lb.Items.AddRange(ls.ToArray());
+        }
+
+        public void RefreshListBox(ListBox lb, string[] sa)
+        {
+            lb.Items.Clear();
+            lb.Items.AddRange(sa);
+        }
+        public void UpdateRessourceFiles(string[] sa, string ressourcePath)
+        {
+            StreamWriter sw = new StreamWriter(ressourcePath, false);
+            foreach (string line in sa)
+            {
+                sw.WriteLine(line);
+            }
+            sw.Close();
+        }
+        public void ListStringAddToOrSwitchToFirst(List<string> lst, string item)
+        {
+            if (lst.Contains(item))
+            {
+                lst.Remove(item);
+            }
+            lst.Insert(0, item);
+        }
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string specialFolder = AppDomain.CurrentDomain.BaseDirectory + @"LauchPadFiles";
+            string MRUfilesPath = specialFolder + @"\MRUfiles.txt";
+            string MRUfoldersPath = specialFolder + @"\MRUfolders.txt";
+
+            if (!Directory.Exists(specialFolder))
+            {
+                Directory.CreateDirectory(specialFolder);
+                File.CreateText(MRUfilesPath);
+                File.CreateText(MRUfoldersPath);
+            }
+
+            StreamReader folderSR = File.OpenText(MRUfoldersPath);
+            RessourceToListAndListBox(folderSR, lstFolders, folderList);
+
+            StreamReader filesSR = File.OpenText(MRUfilesPath);
+            RessourceToListAndListBox(filesSR, lstFiles, fileList);
+            folderSR.Close();
+            filesSR.Close();
+        }
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog oFile = new OpenFileDialog();
+            oFile.Title = "Choose the file to be launched.";
+            oFile.InitialDirectory = Environment.SpecialFolder.MyComputer.ToString();
+            if(oFile.ShowDialog() == DialogResult.OK)
+            {
+                String newFile = oFile.FileName;
+                ListStringAddToOrSwitchToFirst(fileList, newFile);
+                RefreshListBox(lstFiles, fileList.ToArray());
+
+                // get the new folder Path
+
+                int lastSlash = newFile.LastIndexOf("\\");
+                string newFolder = newFile.Substring(0, lastSlash);
+
+                // for it is a root folder, slash\ is added for the sake consistency.
+
+                if (!newFolder.Contains("\\"))
+                {
+                    newFolder = newFolder + "\\";
+                    
+                }
+                ListStringAddToOrSwitchToFirst(folderList, newFolder);
+                RefreshListBox(lstFolders, folderList.ToArray());
+
+                // launch the user selected file
+                Process pro = new Process();
+                pro.StartInfo.FileName = newFile;
+                pro.StartInfo.UseShellExecute = true;
+                pro.Start();
+            }
+        }
+
+        private void lstFolders_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(lstFolders.SelectedItem != null)
+            {
+                if (Directory.Exists(lstFolders.SelectedItem.ToString()))
+                {
+                    // open Folder in window Explorer
+                    Process.Start(@lstFolders.SelectedItem.ToString());
+                    ListStringAddToOrSwitchToFirst(folderList, lstFolders.SelectedItem.ToString());
+                    RefreshListBox(lstFolders, folderList.ToArray());
+                }
+                else
+                {
+                    folderList.Remove(lstFolders.SelectedItem.ToString());
+                    RefreshListBox(lstFolders, folderList.ToArray());
+                }
+            }
+        }
+
+        private void lstFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           if(lstFiles.SelectedItem != null)
+            {
+                if (File.Exists(lstFiles.SelectedItem.ToString()))
+                {
+                    // launch the selected files
+                    Process pro = new Process();
+                    pro.StartInfo.FileName = lstFiles.SelectedItem.ToString();
+                    pro.StartInfo.UseShellExecute = true;
+                    pro.Start();
+                    ListStringAddToOrSwitchToFirst(fileList, lstFiles.SelectedItem.ToString());
+                    RefreshListBox(lstFiles, fileList.ToArray());
+                }
+                else
+                {
+                    fileList.Remove(lstFolders.SelectedItem.ToString());
+                    RefreshListBox(lstFiles, fileList.ToArray());
+                }
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            string specialFolder = AppDomain.CurrentDomain.BaseDirectory + @"LauchPadFiles";
+            string MRUfilesPath = specialFolder + @"\MRUfiles.txt";
+            string MRUfoldersPath = specialFolder + @"\MRUfolders.txt";
+            UpdateRessourceFiles(fileList.ToArray(), MRUfilesPath);
+            UpdateRessourceFiles(folderList.ToArray(), MRUfoldersPath);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
